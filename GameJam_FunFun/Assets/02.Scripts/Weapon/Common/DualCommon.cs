14a0813461b466ba292dce2637eba81662c9515c
snowgaru <77821586+snowgaru@users.noreply.github.com>
2022-08-20 14:40:43 +0900

    펀치공격완료

--- GameJam_FunFun/Assets/02.Scripts/Weapon/Common/DualCommon.cs
+++ GameJam_FunFun/Assets/02.Scripts/Weapon/Common/DualCommon.cs
@@ -8,19 +8,19 @@ public class DualCommon : MonoBehaviour
     void OnEnable()
     {
         //this.transform.position = new Vector3(transform.position.x, 1f, 0);
-        Invoke("PlayAnimation", 0.25f);
+        Invoke("PlayAnimation", 0.15f);
     }
 
     public void PlayAnimation()
     {
         if (this.transform.name == "Dual_1_1")
         {
-            transform.DOLocalMove(new Vector3(this.transform.localPosition.x - 2f, transform.transform.localPosition.y - 2f, 0), 0.7f).OnComplete(() => { End(); });
+            transform.DOLocalMove(new Vector3(this.transform.localPosition.x + 2f, transform.transform.localPosition.y + 2f, 0), 0.45f).OnComplete(() => { End(); });
         }
 
         if (this.transform.name == "Dual_1_2")
         {
-            transform.DOLocalMove(new Vector3(this.transform.localPosition.x + 2f, transform.transform.localPosition.y - 2f, 0), 0.7f).OnComplete(() => { End(); });
+            transform.DOLocalMove(new Vector3(this.transform.localPosition.x - 2f, transform.transform.localPosition.y + 2f, 0), 0.45f).OnComplete(() => { End(); });
         }
     }
 
